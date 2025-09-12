99b8bcd9f2613784533627d75b90435861e6b6fb
najwanzaky <najwanzaky21@gmail.com>
2025-09-12 15:24:50 +0700

    Add HeadOfFamily model and migration for head_of_families table

---
+++ Backend/app/Models/HeadOfFamily.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\UUID;
+
+class HeadOfFamily extends Model
+{
+    use SoftDeletes;
+    use UUID;
+
+    protected $table = 'head_of_families';
+    protected $fillable = [
+        'user_id',
+        'profile_picture',
+        'identity_number',
+        'gender',
+        'birth_date',
+        'phone_number',
+        'occupation',
+        'marital_status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function familyMembers()
+    {
+        return $this->hasMany(FamilyMember::class);
+    }
+}
---
+++ Backend/database/migrations/2025_09_12_075828_create_head_of_families_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('head_of_families', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('profile_picture')->nullable();
+            $tabel->int('inditity_number')->unique();
+            $table->enum('gender', ['male', 'female']);
+            $table->date('birth_date');
+            $tables->string('phone_number')->nullable();
+            $table->string('occupation')->nullable();
+            $table->enum('marital_status', ['single', 'married']);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('head_of_families');
+    }
+};
