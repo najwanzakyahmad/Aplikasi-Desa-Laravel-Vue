b2de3a7bc9ed4303ae300caf6f1a903db3322fb6
najwanzaky <najwanzaky21@gmail.com>
2025-10-11 07:23:00 +0700

    add sidebar and topbar

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,13 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Dashboard from '@/views/Dashboard.vue'
+import Main from '@/layouts/Main.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      component: Main,
+      children: [
+        {
+          path : '',
+          name: 'dashboard',
+          component: Dashboard
+        },
+      ]
     },
     
   ],
